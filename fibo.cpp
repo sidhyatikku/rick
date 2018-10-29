bd3fb46a8160dc246f49c885ec4b77d5845b9179
Raghav Puri <raghavpuri1234567@gmail.com>
2018-10-29 16:36:35 +0530

    Update fibo.cpp

--- fibo.cpp
+++ fibo.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 using namespace std;
-void main()
-  
-{
+void main(){
 int i,n,x=0,y=1,z;
-
-cout << "Enter nubmer of terms for Fibonacci Series: ";
+cout << "Enter number of terms for Fibonacci Series:";
 cin >> n;
 cout << "Fibonacci series are: \n";
-for(i = 0; i < n; i++)
-  
-{
+for(i = 0; i < n; i++){
 cout << endl << x;
 z=x+y;
 x=y;
